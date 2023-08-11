0fddf8fbc97c5e4b7b30c43caa463ca34ccda07a
udinocera <udinocera@gmail.com>
2023-08-12 00:21:49 +0200

    esercizio

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateAverageAge(persons) {
+    const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
+    const average = totalAge / persons.length;
+    return average;
+  }
+  
+  const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+  const average = calculateAverageAge(persons);
+  console.log(persons);
+  console.log(average);
